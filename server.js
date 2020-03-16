846238c42d1ddc81ca964d28479f041f4da42e4d
Edwin Torres <edwin.torres.jr2@gmail.com>
2020-03-16 15:44:57 -0400

    added user endpoints

--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 const knex = require("knex");
 const express = require("express")
+const http = require("http")
 const port = 5000;
 
 const config = require("./knexfile.js")
+const socketIo = require("socket.io")
+const app = express()
+const server = http.createServer(app);
 
-const server = express();
-
-server.use(express.json())
+app.use(express.json())
 const db = knex(config.development);
+const io = socketIo(server)
 module.exports = db;
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send(":D")
 })
+
+app.get("/names", (req, res) => {
+    db("names")
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+app.post("/names", (req, res) => {
+    let creds = req.body;
+    db("names")
+      .insert(creds)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+  });
 
 server.listen(port, () => {
     console.log(`server listening on port ${port}`)
